53ffcb4962477363f5360dc2bd1aa52174858021
shompa <nico_nahuelpan@hotmail.com>
2020-09-16 01:28:21 -0300

    chage build folder for docs

---
+++ docs/precache-manifest.5d7d5f5105773703020117332c3729d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9425aa192e3dd74a274ad8be4c3a20c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c6d9baf0ae457a524f1",
+    "url": "/static/css/main.05241e3c.chunk.css"
+  },
+  {
+    "revision": "50c5007e2135538c1738",
+    "url": "/static/js/2.a7fcd019.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7fcd019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6d9baf0ae457a524f1",
+    "url": "/static/js/main.60b6de79.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
