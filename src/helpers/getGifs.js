3a94aa859d2e9c023af2fd7b615503f5ba1a16b7
shompa <nico_nahuelpan@hotmail.com>
2020-09-16 01:26:27 -0300

    gif app

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+const apiKey = 'bIICwb9TleP0dPFqBiKDWOyvTodpm64M';
+
+
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=15&api_key=${apiKey}`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  const gifs = data.map(img => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url
+    }
+  });
+  return gifs;
+}
